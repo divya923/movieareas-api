2f6fdfa3ed2c36bc3b5d4e544d17aa9c5f8a2e53
Dhivya <dhivyanisha762@gmail.com>
2026-02-28 12:52:36 +0530

    route structure

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
-import express from 'express';
+import express from "express";
 
-const app=express();
-const PORT=7878
-app.get('/',(req,res)=>{
-     res.json({msg:'hello world!!!'});
+const app = express();
+const PORT = 5000;
+app.get("/", (req, res) => {
+  res.json({ msg: "hello world!!!" });
+});
+//  CRUD
+app.get("/movies", () => {
+  console.log("Get a movie");
 });
 
-app.listen(PORT,()=>{
-    console.log(`app runing at http://localhost:${PORT}`);
+app.post("/movies", () => {
+  console.log("Insert a movie");
+});
+app.put("/movies/:id", () => {
+  console.log("Update a movies");
+});
+app.delete("/movies/:id", () => {
+  console.log("Delete movie");
+});
+app.listen(PORT, () => {
+  console.log(`app runing at http://localhost:${PORT}`);
 });
