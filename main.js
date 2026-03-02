513a02ddc218245b45371286556ac22266d0d2fa
Dhivya <dhivyanisha762@gmail.com>
2026-03-02 13:26:12 +0530

    feat=db connected

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 import express from "express";
 import movieRoutes from './routes/movies.route.js'
-
+import connectDB from "./lib/db.js";
 const app = express();
 const PORT = 5000;
 app.get("/", (req, res) => {
   res.json({ msg: "hello world!!!" });
 });
 
-
+connectDB()
 app.use('/movies',movieRoutes) 
 
 
