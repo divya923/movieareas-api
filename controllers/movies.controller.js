60a619d1b18c2023c6dc1ad0ee0ac50c85bae8a9
Dhivya <dhivyanisha762@gmail.com>
2026-03-02 13:36:32 +0530

    understanding middlewere

--- controllers/movies.controller.js
+++ controllers/movies.controller.js
@@ -1,15 +1,16 @@
-export const MovieIndex=(req,res)=>{
-    res.send("Get a Moview");
-}
+export const MovieIndex = (req, res) => {
+  res.send("Get a Moview");
+};
 
-export const CreateMovie=(req,res) => {
-  res.send("Insert a movie");
-}
+export const CreateMovie = (req, res) => {
+  console.log(req.body);
+  return res.json(req.body);
+};
 
-export const UpdateMovie= (req,res) => {
+export const UpdateMovie = (req, res) => {
   res.send("Update a movies");
-}
+};
 
-export const DeleteMovie= (req,res) => {
+export const DeleteMovie = (req, res) => {
   res.send("Delete movie");
-}
+};
--- main.js
+++ main.js
@@ -1,19 +1,29 @@
 import express from "express";
 import movieRoutes from './routes/movies.route.js'
 import connectDB from "./lib/db.js";
+
+
+
+
 const app = express();
 const PORT = 5000;
+
+
+
+//data understanding middlewere
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+//connect DB
+connectDB()
+
 app.get("/", (req, res) => {
   res.json({ msg: "hello world!!!" });
 });
 
-connectDB()
+
+
 app.use('/movies',movieRoutes) 
-
-
-
-
-
 app.listen(PORT, () => {
   console.log(`app runing at http://localhost:${PORT}`);
 });
